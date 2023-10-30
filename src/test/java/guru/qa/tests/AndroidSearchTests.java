347d8accf68b67be2210b401d607470caa9f81d7
Valeria Reshetina <dmitry.potapov@icloud.com>
2023-10-30 12:19:27 +0300

    fighting against AndroidTests continues

--- src/test/java/guru/qa/tests/AndroidSearchTests.java
+++ src/test/java/guru/qa/tests/AndroidSearchTests.java
@@ -18,21 +18,32 @@ public class AndroidSearchTests extends TestBase {
     @Tag("android")
     @DisplayName("Checking search in Wikipedia")
     void successfulWikiSearchTest() {
-
-        step("Click on the search input", () -> {
+        step("Type search", () -> {
             $(accessibilityId("Search Wikipedia")).click();
+            $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Appium");
         });
+        step("Verify content found", () ->
+                $$(id("org.wikipedia.alpha:id/page_list_item_title"))
+                        .shouldHave(sizeGreaterThan(0)));
+    }
 
-        step("Type in the search input Depeche Mode", () -> {
-            $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Depeche Mode");
+    @Test
+    @Tag("android")
+    @DisplayName("Error when going to Wiki article")
+    void checkErrorMessage() {
+        step("Type search", () -> {
+            $(accessibilityId("Search Wikipedia")).click();
+            $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Appium");
         });
-
-        step("Checking for content in search results", () ->
+        step("Verify content found", () ->
                 $$(id("org.wikipedia.alpha:id/page_list_item_title"))
                         .shouldHave(sizeGreaterThan(0)));
 
-        step("Open item from a search result", () ->
-                $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_container")).first().click());
+        step("Open the first item from the search result", () ->
+                $$(id("org.wikipedia.alpha:id/page_list_item_container")).first().click());
 
+        step("Check error message in result", () -> {
+            $(id("org.wikipedia.alpha:id/view_wiki_error_text")).shouldBe(visible);
+        });
     }
 }

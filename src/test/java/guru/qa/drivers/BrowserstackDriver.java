0212ebeef8cf88f4b626d7e2b35def7f056047cf
Valeria Reshetina <dmitry.potapov@icloud.com>
2023-10-30 11:56:28 +0300

    upd BrowserstackDriver

--- src/test/java/guru/qa/drivers/BrowserstackDriver.java
+++ src/test/java/guru/qa/drivers/BrowserstackDriver.java
@@ -1,6 +1,9 @@
 package guru.qa.drivers;
 
 import com.codeborne.selenide.WebDriverProvider;
+import guru.qa.config.MobileConfig;
+import guru.qa.config.UserConfig;
+import org.aeonbits.owner.ConfigFactory;
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
@@ -14,18 +17,22 @@ public class BrowserstackDriver implements WebDriverProvider {
     @Nonnull
     @Override
     public WebDriver createDriver(@Nonnull Capabilities capabilities) {
+
+        MobileConfig mobile = ConfigFactory.create(MobileConfig.class, System.getProperties());
+        UserConfig userConfig = ConfigFactory.create(UserConfig.class, System.getProperties());
+
         MutableCapabilities mutableCapabilities = new MutableCapabilities();
 
         // Set your access credentials
-        mutableCapabilities.setCapability("browserstack.user", "mobile_nbwJlO");
-        mutableCapabilities.setCapability("browserstack.key", "1Ka6UHvjMiGayuohvqnJ");
+        mutableCapabilities.setCapability("browserstack.user", userConfig.getUsername());
+        mutableCapabilities.setCapability("browserstack.key", userConfig.getPassword());
 
         // Set URL of the application under test
-        mutableCapabilities.setCapability("app", "bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c");
+        mutableCapabilities.setCapability("app", mobile.getApp());
 
         // Specify device and os_version for testing
-        mutableCapabilities.setCapability("device", "Google Pixel 3");
-        mutableCapabilities.setCapability("os_version", "9.0");
+        mutableCapabilities.setCapability("device", mobile.getDevice());
+        mutableCapabilities.setCapability("os_version", mobile.getVersion());
 
         // Set other BrowserStack capabilities
         mutableCapabilities.setCapability("project", "First Java Project");
@@ -36,7 +43,7 @@ public class BrowserstackDriver implements WebDriverProvider {
         // and desired capabilities defined above
         try {
             return new RemoteWebDriver(
-                    new URL("https://hub.browserstack.com/wd/hub"), mutableCapabilities);
+                    new URL(userConfig.getRemoteUrl()), mutableCapabilities);
         } catch (MalformedURLException e) {
             throw new RuntimeException(e);
         }

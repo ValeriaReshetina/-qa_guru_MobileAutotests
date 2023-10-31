30fc942cfabb58c671b292a680cdd62bf6a4c871
Valeria Reshetina <dmitry.potapov@icloud.com>
2023-10-31 12:53:07 +0300

    second test

--- src/test/java/guru/qa/tests/AndroidSearchTests.java
+++ src/test/java/guru/qa/tests/AndroidSearchTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 import static io.appium.java_client.AppiumBy.accessibilityId;
@@ -23,5 +24,25 @@ public class AndroidSearchTests extends TestBase {
         step("Verify content found", () ->
                 $$(id("org.wikipedia.alpha:id/page_list_item_title"))
                         .shouldHave(sizeGreaterThan(0)));
+    }
+
+    @Test
+    @Tag("android")
+    @DisplayName("Error when going to Wikipedia article")
+    void checkErrorMessage() {
+        step("Type search", () -> {
+            $(accessibilityId("Search Wikipedia")).click();
+            $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Appium");
+        });
+        step("Verify content found", () ->
+                $$(id("org.wikipedia.alpha:id/page_list_item_title"))
+                        .shouldHave(sizeGreaterThan(0)));
+
+        step("Open the first item from the search result", () ->
+                $$(id("org.wikipedia.alpha:id/page_list_item_container")).first().click());
+
+        step("Check error message in result", () -> {
+            $(id("org.wikipedia.alpha:id/view_wiki_error_text")).shouldBe(visible);
+        });
     }
 }
